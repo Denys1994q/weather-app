ec0fcc1ea3d4ba9e06407113a4762e3e992ad2af
Denys1994q <drybachok@gmail.com>
2024-02-21 11:19:23 +0200

    add scroll carousel for city cards

--- src/components/city-cards/City-cards.tsx
+++ src/components/city-cards/City-cards.tsx
@@ -1,32 +1,62 @@
-import './City-cards.css'
-import CityCard from '../city-card/City-card'
-import { City } from '../../data/citiesData'
-import { transformDate } from '../../utils/dateUtils'
+import React, { useRef } from "react";
+import "./City-cards.css";
+import CityCard from "../city-card/City-card";
+import { City } from "../../data/citiesData";
+import { transformDate } from "../../utils/dateUtils";
 
 interface CityCardsProps {
     cities: City[];
-    onCityClick: (cityId: string) => void; 
+    onCityClick: (cityId: string) => void;
     activeCityId: string;
 }
 
-const CityCards: React.FC<CityCardsProps> = ({cities, activeCityId, onCityClick}) => {
-    const handleCityClick = (cityId: string) => {
-        onCityClick(cityId); 
-    };
-    return (
-        <ul className="cards">
-            { cities.map(c => <li key={c.id}>
-                <CityCard 
-                    img={c.img} 
-                    title={c.city} 
-                    startDate={transformDate(c.startDate)}
-                    endDate={transformDate(c.endDate)}
-                    isActive={c.id === activeCityId} 
-                    onClick={() => handleCityClick(c.id)} />
-                </li>
-            )}
-        </ul>
-    )
-}
+const CityCards: React.FC<CityCardsProps> = ({ cities, activeCityId, onCityClick }) => {
+    const containerRef = useRef<HTMLUListElement>(null);
 
-export default CityCards
+    const handleCityClick = (cityId: string) => {
+        onCityClick(cityId);
+    };
+
+    const scrollToLeft = () => {
+        if (containerRef.current) {
+            containerRef.current.scrollLeft -= containerRef.current.offsetWidth;
+        }
+    };
+
+    const scrollToRight = () => {
+        if (containerRef.current) {
+            containerRef.current.scrollLeft += containerRef.current.offsetWidth;
+        }
+    };
+
+    return (
+        <div className='carousel-container'>
+            {cities.length > 0 ? (
+                <>
+                    <div className='carousel__btns'>
+                        <button className='carousel__btn prev' onClick={scrollToLeft}></button>
+                        <button className='carousel__btn next' onClick={scrollToRight}></button>
+                    </div>
+                    <ul ref={containerRef} className='carousel'>
+                        {cities.map(c => (
+                            <li key={c.id} className='carousel-card'>
+                                <CityCard
+                                    img={c.img}
+                                    title={c.city}
+                                    startDate={transformDate(c.startDate)}
+                                    endDate={transformDate(c.endDate)}
+                                    isActive={c.id === activeCityId}
+                                    onClick={() => handleCityClick(c.id)}
+                                />
+                            </li>
+                        ))}
+                    </ul>
+                </>
+            ) : (
+                <h4>Nothing found</h4>
+            )}
+        </div>
+    );
+};
+
+export default CityCards;
